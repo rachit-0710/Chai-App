90c942484e5a752399d293101e36c2f53cc0f1cb
Rachit Agarwal <153610942+rachit-0710@users.noreply.github.com>
2024-01-02 00:55:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Oops, forgot the JavaScript again.
+
+// The point of these concepts is to push the limits of CSS and realize what its capabilities and limitations are before implementing JavaScript for stateful interaction.
+// Then, we can truly have separation of concerns without being tempted to use JavaScript for everything.
+
+// Also, click event handlers and state management *should* always be done in JavaScript. =)
+
+// Twitter: @davidkpiano
